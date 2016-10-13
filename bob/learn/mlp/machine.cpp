7eba8dd755c34db74092c37aaec8b704d6a3f190
Andre Anjos <andre.anjos@idiap.ch>
2016-10-13 16:17:17 +0200

    Fix documentation errors

--- bob/learn/mlp/machine.cpp
+++ bob/learn/mlp/machine.cpp
@@ -868,7 +868,7 @@ PyDoc_STRVAR(s_load_str, "load");
 PyDoc_STRVAR(s_load_doc,
 "o.load(f) -> None\n\
 \n\
-Loads itself from a :py:class:`bob.io.HDF5File`\n\
+Loads itself from a :py:class:`bob.io.base.HDF5File`\n\
 \n\
 ");
 
@@ -902,7 +902,7 @@ PyDoc_STRVAR(s_save_str, "save");
 PyDoc_STRVAR(s_save_doc,
 "o.save(f) -> None\n\
 \n\
-Saves itself at a :py:class:`bob.io.HDF5File`\n\
+Saves itself at a :py:class:`bob.io.base.HDF5File`\n\
 \n\
 ");
 
